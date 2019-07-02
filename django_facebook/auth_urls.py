1729d829c578b53e9d0988a412006667f84b18d4
Mihai Oprea <mishu@mishulica.com>
2019-07-02 15:26:08 +0300

    Update auth_urls.py
    Updated auth views for django 1.9+

--- django_facebook/auth_urls.py
+++ django_facebook/auth_urls.py
@@ -24,51 +24,49 @@ consult a specific backend's documentation for details.
 """
 
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import *
 from django_facebook import registration_views
 from django_facebook.utils import replication_safe
 
 urlpatterns = [
     url(
         r'^login/$',
-        replication_safe(auth_views.login),
-        {'template_name': 'registration/login.html'},
+        replication_safe(LoginView.as_view(template_name='users/login.html'),
         name='auth_login'
     ),
     url(
         r'^logout/$',
-        replication_safe(auth_views.logout),
-        {'template_name': 'registration/logout.html'},
+        replication_safe(LogoutView.as_view(template_name='users/logout.html'),
         name='auth_logout'
     ),
     url(
         r'^password/change/$',
-        auth_views.password_change,
+        PasswordChangeView.as_view(),
         name='auth_password_change'
     ),
     url(
         r'^password/change/done/$',
-        auth_views.password_change_done,
+        PasswordChangeDoneView.as_view(),
         name='auth_password_change_done'
     ),
     url(
         r'^password/reset/$',
-        auth_views.password_reset,
+        PasswordResetView.as_view(),
         name='auth_password_reset'
     ),
     url(
         r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.password_reset_confirm,
+        PasswordResetConfirmView.as_view(),
         name='auth_password_reset_confirm'
     ),
     url(
         r'^password/reset/complete/$',
-        auth_views.password_reset_complete,
+        PasswordResetCompleteView.as_view(),
         name='auth_password_reset_complete'
     ),
     url(
         r'^password/reset/done/$',
-        auth_views.password_reset_done,
+        PasswordResetDoneView.as_view(),
         name='auth_password_reset_done'
     ),
     url(
